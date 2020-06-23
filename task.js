092e60b5ee0197ae753bccfd2090671f51e0338e
Daniil <trusenevdaniil@yandex.ru>
2020-06-23 22:05:56 +0500

    second commit

--- task.js
+++ task.js
@@ -2,23 +2,20 @@
 
 function getResult(a,b,c){
     let discriminant = b**2-4*a*c;
-    let x1, x2;
+    let result = [];
 
     if(discriminant > 0) {
-        x1 = (-b + Math.sqrt(discriminant))/(2*a);
-        x2 = (-b - Math.sqrt(discriminant))/(2*a);
-        return x1, x2;
-    }  else if(discriminant == 0) {
-        x1 = -b/(2*a);
-        return x1;
-    } else if(discriminant < 0) {
-        return "Корней нет";
+      result[0] = (-b + Math.sqrt(discriminant))/(2*a);
+      result[1] = (-b - Math.sqrt(discriminant))/(2*a);
+    } else if(discriminant == 0) {
+      result = [-b / (2 * a)];
     }
+
+    return result;
 }
 
 function getAverageMark(marks) {
     let sum = 0;
-    let averageMark;
   
     if(marks.length == 0) {
       return 0;
@@ -27,9 +24,8 @@ function getAverageMark(marks) {
     }
     for(let i = 0; i < marks.length; i++) {
       sum += marks[i];
-      averageMark = sum / marks.length;
     }
-    return averageMark;
+    return sum / marks.length;
   }
 
 function askDrink(name,dateOfBirthday){
